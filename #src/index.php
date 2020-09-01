44da641e1e8cbb15aafc86d410ca0b817879f24a
stillraider <61648212+stillraider@users.noreply.github.com>
2020-09-01 12:44:35 +0500

    completed the upper block
    there was a problem with fonts, I had to spend time

---
+++ #src/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.min.css">
+    <title>Sub Sandwiches - Breakfast, Sandwiches, Salads & More | SUBWAY® | SUBWAY.com - United States (English)</title>
+</head>
+<body>
+    <section class="inbetweener">
+        <div class="inbetweener__container">
+            @@include("_header.html")
+            <div class="inbetweener__body">
+                <div class="inbetweener__img"></div>
+                <div class="inbetweener__contant">
+                    <div class="promote">
+                        <h1 class="promote__title">New Automation Tool for Your Home</h1>
+                        <p class="promote__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Faucibus tristique vulputate ultrices ut mauris tellus at. Posuere sollicitudin odio tellus elit.</p>
+                        <button class="promote__button"><span>See Our Project</span></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+    <script src="scripts/script.min.js" defer></script>
+</body>
+</html>
